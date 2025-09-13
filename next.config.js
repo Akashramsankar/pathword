36f9ebca36faedd9f4ba1de6a89f84cdec31f759
Akashramsankar <akasram@gmail.com>
2025-09-13 23:49:13 +0530

    Added ques & pathword.co changes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/pathword' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/pathword/' : '',
+  // basePath: process.env.NODE_ENV === 'production' ? '/pathword' : '',
+  // assetPrefix: process.env.NODE_ENV === 'production' ? '/pathword/' : '',
   images: { unoptimized: true },
   trailingSlash: true,
 }
